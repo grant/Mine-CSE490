a9d744bb7c8dd5297b5df60c5f0eae2359e46e86
Grant Timmerman <granttimmerman@gmail.com>
2013-11-21 16:15:46 -0800

    Finished home page

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(function() {
+	var FADE_DURATION = 200;
+
+	function setup () {
+		show('home', 0);
+
+		// Setup click handlers
+		$('.createBtn').click(function() {
+			show('create');
+		});
+		$('.homeBtn').click(function() {
+			show('home');
+		});
+		$('.analyticsBtn').click(function() {
+			show('analytics');
+		});
+	}
+
+	function show(name, fadeTime) {
+		// Set the hashtag
+		window.location.hash = name;
+
+		// Fade to page
+		if (typeof fadeTime === 'undefined') {
+			fadeTime = FADE_DURATION;
+		}
+		$('#home').fadeOut(fadeTime);
+		$('#analytics').fadeOut(fadeTime);
+		$('#create').fadeOut(fadeTime);
+		$('#' + name).fadeIn(fadeTime);
+	}
+
+	setup();
+});
+
