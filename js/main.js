24062ccedffa292c8d64ca16cb34353e60f0b611
Grant Timmerman <granttimmerman@gmail.com>
2013-12-02 10:34:45 -0800

    Added image upload button

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function() {
+	var PREFIX = "img/uploads/";
 	var FADE_DURATION = 200;
 
 	function setup () {
@@ -14,6 +15,31 @@ $(function() {
 		$('.analyticsBtn').click(function() {
 			show('analytics');
 		});
+
+
+		// Uploading a pic
+		$("a#picUpload").bind('click', function() {
+			$(":input[type=file]").click();
+			return false;
+		});
+
+		$('#fileUpload').change(function() {
+			var fullPath = getFullPath();
+			console.log(fullPath);
+			$('#uploadedPic').attr('src', fullPath);
+		});
+	}
+
+	function getFullPath () {
+		var fullPath = document.getElementById('fileUpload').value;
+		if (fullPath) {
+			var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+			var filename = fullPath.substring(startIndex);
+			if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+				filename = filename.substring(1);
+			}
+			return PREFIX + filename;
+		}
 	}
 
 	function show(name, fadeTime) {
